4046b7a9054da4ce3096920139aa4aaad7be6791
UWgVXGPsoC <38557680+UWgVXGPsoC@users.noreply.github.com>
2019-04-01 09:28:14 +0200

    Refinado y comentado todo el codigo

--- update.go
+++ update.go
@@ -7,26 +7,32 @@ import (
 	"os/exec"
 )
 
+// Actualiza Krypton a la versión mas reciente disponible
+// en el servidor de actualizaciones
 func update() {
 	if isUpdateAvailable() {
 		log.Println("Hay nueva versión disponible")
 
-		path := "C:\\Program Files\\Krypton\\Updates\\Krypton.exe"
+		path := "C:/Program Files/Krypton/Updates/Krypton.exe"
 		url := "https://paesacybersecurity.eu/krypton/Krypton.exe"
 		err := downloadToFile(url, path)
 		if err != nil {
 			log.Fatal("Error al descargar actualización")
 		}
 		log.Println("Actualización descargada correctamente")
+
+		// Ejecutar la nueva versión para que se instale
 		cmd := exec.Command(path, "--install")
 		err = cmd.Start()
-
 	} else {
 		log.Println("No hay nueva versión disponible")
 	}
 
 }
 
+// Comprueba si hay una nueva versión de Krypton disponible
+// comparando su versión con la que contiene el archivo krypton.version
+// descargado del servidor
 func isUpdateAvailable() bool {
 	resp, err := http.Get("https://paesacybersecurity.eu/krypton/krypton.version")
 	if err != nil {
