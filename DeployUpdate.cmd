2665d8671321346b4ee43087c04ae870e8eb39d0
milgradesec <38557680+milgradesec@users.noreply.github.com>
2020-01-19 08:30:16 +0100

    fix path

--- DeployUpdate.cmd
+++ DeployUpdate.cmd
@@ -1,2 +1,2 @@
-scp Krypton.exe pi@192.168.1.100:/var/www/dlserver/krypton/stable/Krypton.exe
-scp krypton.version pi@192.168.1.100:/var/www/dlserver/krypton/stable/krypton.version
+scp Krypton.exe ubuntu@192.168.1.100:/var/www/dlserver/krypton/stable/Krypton.exe
+scp krypton.version ubuntu@192.168.1.100:/var/www/dlserver/krypton/stable/krypton.version
