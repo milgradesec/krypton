c90df279977525b9b6ba78f93f3f5882cb3f65cc
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-02-01 13:00:51 +0100

    Create custom http.Client with hardened defaults

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -2,10 +2,12 @@ package updater
 
 import (
 	"crypto"
+	"crypto/tls"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
 	"runtime"
 	"time"
@@ -49,7 +51,26 @@ type serverResponse struct {
 
 var (
 	client = &http.Client{
-		Timeout: 15 * time.Second, //nolint
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).Dial,
+
+			TLSClientConfig: &tls.Config{
+				MinVersion: tls.VersionTLS12,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				},
+			},
+			TLSHandshakeTimeout: 10 * time.Second,
+		},
+		Timeout: 15 * time.Second,
 	}
 )
 
